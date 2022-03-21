c8b28873c7519b82dff8076240205346ab5f2402
brandodo <brandonong93@gmail.com>
2022-03-20 22:22:06 -0400

    completed post to add new inventory item

---
+++ routes/inventories.js
@@ -0,0 +1,38 @@
+const express = require("express");
+const fs = require("fs");
+const router = express.Router();
+const { v4: uuidv4 } = require("uuid");
+
+const INVENTORY_DATA = "./data/inventories.json";
+
+// Add New Inventory Item
+router.post("/add", (req, res) => {
+  const { itemName, itemDescription, category, status, quantity, warehouse } = req.body;
+
+  const inventoryJSON = {
+    id: uuidv4(),
+    warehouseID: uuidv4(),
+    warehouseName: warehouse,
+    itemName: itemName,
+    description: itemDescription,
+    category: category,
+    status: status,
+    quantity: quantity,
+  };
+
+  fs.readFile(INVENTORY_DATA, "utf-8", (err, data) => {
+    if (err) throw err;
+
+    const currentData = JSON.parse(data);
+    currentData.push(inventoryJSON);
+
+    fs.writeFile(INVENTORY_DATA, JSON.stringify(currentData), (err) => {
+      if (err) throw err;
+
+      console.log("New inventory item added!");
+      res.status(200).send(inventoryJSON);
+    });
+  });
+});
+
+module.exports = router;
--- server.js
+++ server.js
@@ -1,10 +1,14 @@
-const express = require('express');
-const cors = require('cors');
+const express = require("express");
+const cors = require("cors");
 const app = express();
 
+const inventoryRoutes = require("./routes/inventories");
+
+app.use(express.json());
 app.use(cors());
 
+app.use("/inventory", inventoryRoutes);
 
 app.listen(8080, function () {
-  console.log('CORS-enabled web server listening on port 8080')
+  console.log("CORS-enabled web server listening on port 8080");
 });
