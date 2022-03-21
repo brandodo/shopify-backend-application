3118b9fd1c8c1b00d577ab7d8d13db825dad77cd
brandodo <brandonong93@gmail.com>
2022-03-20 22:45:13 -0400

    completed put for editing inventory item

--- routes/inventories.js
+++ routes/inventories.js
@@ -7,7 +7,8 @@ const INVENTORY_DATA = "./data/inventories.json";
 
 // Add New Inventory Item
 router.post("/add", (req, res) => {
-  const { itemName, itemDescription, category, status, quantity, warehouse } = req.body;
+  const { itemName, itemDescription, category, status, quantity, warehouse } =
+    req.body;
 
   const inventoryJSON = {
     id: uuidv4(),
@@ -32,6 +33,42 @@ router.post("/add", (req, res) => {
       console.log("New inventory item added!");
       res.status(200).send(inventoryJSON);
     });
+  });
+});
+
+// Edit Inventory Item
+router.put("/edit/:inventoryId/:warehouseId", (req, res) => {
+  const { itemName, itemDescription, category, status, quantity, warehouse } =
+    req.body;
+
+  const { inventoryId, warehouseId } = req.params;
+
+  fs.readFile(INVENTORY_DATA, (err, data) => {
+    if (err) throw err;
+
+    const currentData = JSON.parse(data);
+    const invIndex = currentData.findIndex(
+      (video) => video.id === inventoryId && video.warehouseID === warehouseId
+    );
+
+    if (invIndex === -1) {
+      res.status(404).send("Inventory or Warehouse not found!");
+    } else {
+      const inventoryToUpdate = currentData[invIndex];
+      inventoryToUpdate.itemName = itemName;
+      inventoryToUpdate.description = itemDescription;
+      inventoryToUpdate.category = category;
+      inventoryToUpdate.status = status;
+      inventoryToUpdate.quantity = quantity;
+      inventoryToUpdate.warehouseName = warehouse;
+
+      fs.writeFile(INVENTORY_DATA, JSON.stringify(currentData), (err) => {
+        if (err) throw err;
+
+        console.log("Inventory updated!");
+        res.status(200).send(inventoryToUpdate);
+      });
+    }
   });
 });
 
