602b5ff26d7e4044fb2c40a141a957e78fe6ebfc
brandodo <brandonong93@gmail.com>
2022-03-20 23:09:25 -0400

    completed edit warehouse back-end

--- routes/inventories.js
+++
@@ -1,6 +0,0 @@
-const express = require("express");
-const fs = require("fs");
-const router = express.Router();
-const { v4: uuidv4 } = require("uuid");
-
-const INVENTORY_DATA = "./data/inventories.json";
--- server.js
+++ server.js
@@ -1,10 +1,16 @@
-const express = require('express');
-const cors = require('cors');
+const express = require("express");
+const cors = require("cors");
 const app = express();
 
+const inventoryRoutes = require("./routes/inventories");
+const warehouseRoutes = require("./routes/warehouse");
+
+app.use(express.json());
 app.use(cors());
 
+// app.use("/inventory", inventoryRoutes);
+app.use("/warehouse", warehouseRoutes);
 
 app.listen(8080, function () {
-  console.log('CORS-enabled web server listening on port 8080')
+  console.log("CORS-enabled web server listening on port 8080");
 });
